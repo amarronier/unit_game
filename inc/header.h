d1fe0e823a776409517fdab2a9ab3052162ff5d2
Alina Gruver <agruver@e2r7p3.unit.ua>
2020-08-23 19:02:53 +0300

    header

---
+++ inc/header.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+#include <stdio.h>
+#include <stddef.h>
+
+#define FALSE 0
+#define TRUE 1
+
+#define WINDOW_WIDHT 1280
+#define WINDOW_HEIGHT 720
+
+#define FPS 60
+#define FRAME_TARGET_TIME (1000 / FPS)
